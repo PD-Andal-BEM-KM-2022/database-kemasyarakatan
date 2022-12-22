79222dd20853c6ae549775929fe320736a0e1bdc
hadzami10 <achmad.had2003@mail.ugm.ac.id>
2022-12-22 13:12:30 +0700

    add comment api

--- pages/api/post/get-posts.ts
+++ pages/api/post/get-posts.ts
@@ -6,10 +6,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const db = client.db();
   const collection = db.collection("posts");
   const limit = req.query.limit || 10;
+  const category = req.query.category || "";
   const page = (Number(req.query.page) - 1) * Number(limit) || 0;
-  const maxPage = Math.ceil((await collection.countDocuments()) / Number(limit));
+  const maxPage = Math.ceil(
+    (await collection.countDocuments()) / Number(limit)
+  );
 
-  const posts = await collection.find({}).toArray();
+  const posts = await collection
+    .find({
+      category: { $regex: category, $options: "i" },
+    })
+    .toArray();
 
   posts.forEach(post => {
     post.id = post._id;
@@ -20,13 +27,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   if (!posts) return res.status(404).json({ message: "Posts is empty" });
 
-  if(page >= posts.length) return res.status(200).json({ message: "Page not valid" });
+  if (page >= posts.length && page)
+    return res.status(200).json({ message: "Page not valid" });
 
   if (limit > posts.length)
-    return res.status(200).json({ length: posts.length, posts});
+    return res.status(200).json({ length: posts.length, posts });
   else if (limit < 0)
-    return res.status(200).json({ length: posts.length, posts});
+    return res.status(200).json({ length: posts.length, posts });
   else if (limit > 0 && limit < posts.length)
-    return res.status(200).json({maxPage, posts: posts.slice(page, page + Number(limit))});
+    return res
+      .status(200)
+      .json({ maxPage, posts: posts.slice(page, page + Number(limit)) });
   else return res.status(200).json({ message: "Limit not valid" });
 };
